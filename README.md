27220c7cb8ab5adb9350ad58ac5afeeb9adcd26a
Jon C <me@jonc.dev>
2025-02-07 23:29:38 +0100

    CI: Add manual workflow to publish crates (#6)
    #### Problem
    
    The solana-sdk repo has no way of publishing crates.
    
    #### Summary of changes
    
    Following the model used by SPL at
    https://github.com/solana-labs/solana-program-library/pull/7184, add a
    manual workflow to publish crates.

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ To patch all of the crates in this repo for Agave, just run:
 ```console
 ./scripts/patch-crates-no-header.sh <AGAVE_PATH> <SOLANA_SDK_PATH>
 ```
+
+### Publishing a crate from this repository
+
+Unlike Agave, the solana-sdk crates are versioned independently, and published
+as needed.
+
+If you need to publish a crate, you can use the "Publish Crate" GitHub Action.
+Simply type in the path to the crate directory you want to release, ie.
+`program-entrypoint`, along with the kind of release, either `patch`, `minor`,
+`major`, or a specific version string.
+
+The publish job will run checks, bump the crate version, commit and tag the
+bump, publish the crate to crates.io, and finally create GitHub Release with
+a simple changelog of all commits to the crate since the previous release.
---
+++ scripts/publish-rust.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e
+base="$(dirname "${BASH_SOURCE[0]}")"
+# pacify shellcheck: cannot follow dynamic path
+# shellcheck disable=SC1090,SC1091
+source "$base/read-cargo-variable.sh"
+cd "$base/.."
+
+if [[ -z $1 ]]; then
+  echo 'A package manifest path — e.g. "program" — must be provided.'
+  exit 1
+fi
+PACKAGE_PATH=$1
+if [[ -z $2 ]]; then
+  echo 'A version level — e.g. "patch" — must be provided.'
+  exit 1
+fi
+LEVEL=$2
+DRY_RUN=$3
+
+# Go to the directory
+cd "${PACKAGE_PATH}"
+
+# Get the old version, used with git-cliff
+old_version=$(readCargoVariable version "Cargo.toml")
+package_name=$(readCargoVariable name "Cargo.toml")
+tag_name="${package_name//solana-/}"
+
+# Publish the new version, commit the repo change, tag it, and push it all.
+if [[ -n ${DRY_RUN} ]]; then
+  cargo release "${LEVEL}"
+else
+  cargo release "${LEVEL}" --tag-name "${tag_name}@v{{version}}" --no-confirm --execute
+fi
+
+# Stop here if this is a dry run.
+if [[ -n $DRY_RUN ]]; then
+  exit 0
+fi
+
+# Get the new version.
+new_version=$(readCargoVariable version "Cargo.toml")
+new_git_tag="${tag_name}@v${new_version}"
+old_git_tag="${tag_name}@v${old_version}"
+
+# Expose the new version to CI if needed.
+if [[ -n $CI ]]; then
+  echo "new_git_tag=${new_git_tag}" >> "$GITHUB_OUTPUT"
+  echo "old_git_tag=${old_git_tag}" >> "$GITHUB_OUTPUT"
+fi
