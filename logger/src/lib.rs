ef1ed880489addba1d8093715e39dc35aac2247a
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-03-03 15:29:05 -0800

    add redirect_stderr_to_file to logger

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -2,7 +2,11 @@
 
 use {
     lazy_static::lazy_static,
-    std::sync::{Arc, RwLock},
+    std::{
+        env,
+        sync::{Arc, RwLock},
+        thread::JoinHandle,
+    },
 };
 
 lazy_static! {
@@ -74,4 +78,67 @@ pub fn setup_file_with_default(logfile: &str, filter: &str) {
         .target(env_logger::Target::Pipe(Box::new(file)))
         .build();
     replace_logger(logger);
+}
+
+#[cfg(unix)]
+fn redirect_stderr(filename: &str) {
+    use std::{fs::OpenOptions, os::unix::io::AsRawFd};
+    match OpenOptions::new().create(true).append(true).open(filename) {
+        Ok(file) => unsafe {
+            libc::dup2(file.as_raw_fd(), libc::STDERR_FILENO);
+        },
+        Err(err) => eprintln!("Unable to open {filename}: {err}"),
+    }
+}
+
+// Redirect stderr to a file with support for logrotate by sending a SIGUSR1 to the process.
+//
+// Upon success, future `log` macros and `eprintln!()` can be found in the specified log file.
+pub fn redirect_stderr_to_file(logfile: Option<String>) -> Option<JoinHandle<()>> {
+    // Default to RUST_BACKTRACE=1 for more informative validator logs
+    if env::var_os("RUST_BACKTRACE").is_none() {
+        env::set_var("RUST_BACKTRACE", "1")
+    }
+
+    match logfile {
+        None => {
+            setup_with_default_filter();
+            None
+        }
+        Some(logfile) => {
+            #[cfg(unix)]
+            {
+                use log::info;
+                let mut signals =
+                    signal_hook::iterator::Signals::new([signal_hook::consts::SIGUSR1])
+                        .unwrap_or_else(|err| {
+                            eprintln!("Unable to register SIGUSR1 handler: {err:?}");
+                            std::process::exit(1);
+                        });
+
+                setup_with_default_filter();
+                redirect_stderr(&logfile);
+                Some(
+                    std::thread::Builder::new()
+                        .name("solSigUsr1".into())
+                        .spawn(move || {
+                            for signal in signals.forever() {
+                                info!(
+                                    "received SIGUSR1 ({}), reopening log file: {:?}",
+                                    signal, logfile
+                                );
+                                redirect_stderr(&logfile);
+                            }
+                        })
+                        .unwrap(),
+                )
+            }
+            #[cfg(not(unix))]
+            {
+                println!("logrotate is not supported on this platform");
+                setup_file_with_default(&logfile, solana_logger::DEFAULT_FILTER);
+                None
+            }
+        }
+    }
 }
